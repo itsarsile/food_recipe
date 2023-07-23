0f90bdd789347eee563b4739660a90cf429187c2
Vynn <alfinyahyaalyashi13@gmail.com>
2023-07-23 10:59:52 +0700

    nambah video

--- src/pages/DetailRecipe/DetailRecipe.jsx
+++ src/pages/DetailRecipe/DetailRecipe.jsx
@@ -6,11 +6,25 @@ import Play from '../../assets/image/play.png'
 import Footer from '../../components/footer/Footer'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import Modal from 'react-bootstrap/Modal';
 
 
 const DetailRecipe = () => {
+    const [lgShow, setLgShow] = useState(false);
+
     const { id } = useParams();
     const [recipe, setRecipe] = useState("");
+    const [data, setData] = useState({
+        userId: ""
+    })
+
+    // const handleChange = (e) => {
+    //     setData({
+    //         ...data,
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
     useEffect(() => {
         axios.get(`https://food-recipe-be.onrender.com/recipes/${id}`)
             .then((res) => {
@@ -21,6 +35,7 @@ const DetailRecipe = () => {
                 console.log(err);
             })
     }, [id])
+
     return (
         <>
             <Navbar />
@@ -29,10 +44,16 @@ const DetailRecipe = () => {
                     <div className="col-12">
                         <div className={Styles.wrapper}>
                             <h1 className='mb-5' id={Styles.txt}>{recipe.title}</h1>
-                            <img src={recipe.photo} className={Styles.photo} alt="Logo product" />
-                            <div className={Styles.bton}>
-                                <button className={Styles.save}><i className="fa-regular fa-bookmark fa-lg"></i></button>
-                                <button className={Styles.like}><i className="fa-regular fa-thumbs-up fa-lg"></i></button>
+                            <div className={Styles.pth}>
+                                <img src={recipe.photo} className={Styles.photo} alt="Logo product" />
+                                <div className={Styles.bton}>
+                                    <button className={Styles.save}>
+                                        <i className="fa-regular fa-bookmark fa-lg"></i>
+                                    </button>
+                                    <button className={Styles.like}>
+                                        <i className="fa-regular fa-thumbs-up fa-lg"></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -43,17 +64,28 @@ const DetailRecipe = () => {
                         <h3 className={Styles.ha3}>Ingredients</h3>
                         <div className={Styles.list}>
                             <p className={Styles.text}>{recipe.title}</p>
-                            {/* <p className={Styles.text}> - 2 Egg</p>
-                            <p className={Styles.text}> - 3 Slices Bread</p>
-                            <p className={Styles.text}> - A Little Butter</p>
-                            <p className={Styles.text}> - ⅓ Carton Of Cress</p>
-                            <p className={Styles.text}> - 2-3 Slices Of Tomato Or A Lettuce Leaf And A Slice Of Ham Or Cheese</p>
-                            <p className={Styles.text}> - Crisps , To Serve</p> */}
                             <h3 className={Styles.video}>Video Step</h3>
                             <div className={Styles.lists}>
-                                <button className={Styles.btn}>
+                                <button className={Styles.btn} onClick={() => setLgShow(true)}>
                                     <img src={Play} alt="video" />
                                 </button>
+                                <Modal
+                                    size="lg"
+                                    show={lgShow}
+                                    onHide={() => setLgShow(false)}
+                                    aria-labelledby="example-modal-sizes-title-lg"
+                                >
+                                    <Modal.Header closeButton>
+                                        <Modal.Title id="example-modal-sizes-title-lg">
+                                            Large Modal
+                                        </Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <video controls autoPlay
+                                        src={recipe.video} type="mp4/video/webm" className='w-100'>
+                                        </video>
+                                    </Modal.Body>
+                                </Modal>
                             </div>
                         </div>
                     </div>
